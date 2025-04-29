596abd0050df491e372948d6b1507105e97ec3ab
william-j1 <williamj.inbox@gmail.com>
2025-04-29 09:32:08 +0100

    license into sc

--- server.c
+++ server.c
@@ -1,16 +1,18 @@
 /*
 
-ABOUT:
+Copyright 2024 William Johnson williamj.inbox@gmail.com
 
-Copyright (c) 2024 William Johnson <williamj.inbox@gmail.com>
+Licensed under the Apache License, Version 2.0 (the 
+"License"); you may not use this file except in compliance 
+with the License. You may obtain a copy of the License at
 
-Drops useful summary data concerning an active live data server. May be
-utilised by an autonomous client to sweep across a cluster of servers to
-pinpoint those approaching load capacities and maintain an eye on running
-database processes.
+http://www.apache.org/licenses/LICENSE-2.0
 
-Clients may thereafter be configured to dispatch alerts to admin personnel
-to address these key infrastructural concerns.
+Unless required by applicable law or agreed to in writing, 
+software distributed under the License is distributed on an 
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+either express or implied. See the License for the specific 
+language governing permissions and limitations under the License.
 
 DEPLOYMENT NOTES:
 
